d831377ca6532e804a8075b3b84519ce6e1d1887
Akash <Akashsamanta0571@gmail.com>
2025-08-09 21:52:28 +0530

    lobby socket handlers completed

--- server/src/Sockets/index.js
+++ server/src/Sockets/index.js
@@ -1,15 +1,22 @@
 import socketAuth from "../middlewares/socketAuth";
 import { addOnlineUser, removeOnlineUser } from "../libs/onlineUsers";
+import { lobbySocketHandler } from "./lobby.socket";
 
 export async function socketInitialize(io) {
     socketAuth(io);
     io.on("connection", (socket) => {
         console.log("a user connected");
-        addOnlineUser(socket.userId, socket.id);
-    })
 
-    socket.on("disconnect", (reason) => {
-        console.log("a user disconnected, reason: ", reason);
-        removeOnlineUser(socket.userId);
+        // Add the user to the online users map
+        addOnlineUser(socket.userId, socket.id);
+
+        // Handle socket events
+        lobbySocketHandler(socket, io);
+
+        // Remove the user from the online users map
+        socket.on("disconnect", (reason) => {
+            console.log("a user disconnected, reason: ", reason);
+            removeOnlineUser(socket.userId);
+        })
     })
 }
